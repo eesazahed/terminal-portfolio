c137ebb55ab5159c525c8846c026f5928b096bd9
Eesa Zahed <eesa.zahed@gmail.com>
2026-03-07 20:34:49 -0600

    eesa

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from curses import wrapper
 
 def main(stdscr):
     stdscr.clear()
-    stdscr.addstr("hi")
+    stdscr.addstr(3, 0, "hi")
+    stdscr.addstr(5, 0, "eesa")
     stdscr.refresh()
     stdscr.getch()
 
