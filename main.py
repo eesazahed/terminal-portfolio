289e87e5cee4b357d161faec3d09c585745a4718
Eesa Zahed <eesa.zahed@gmail.com>
2026-03-07 22:29:24 -0600

    green color

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
+import curses
 from curses import wrapper
 
 
-def add_line_of_text(stdscr, text, y):
-    height, width = stdscr.getmaxyx()
-    center_x = width // 2 - len(text) // 2
-    stdscr.addstr(y, center_x, text)
-
-
 def main(stdscr):
+    def add_line_of_text(text, y, color=1):
+        height, width = stdscr.getmaxyx()
+        center_x = width // 2 - len(text) // 2
+        stdscr.addstr(y, center_x, text, curses.color_pair(color))
+
     stdscr.clear()
     stdscr.nodelay(False)
     stdscr.keypad(True)
+
+    curses.start_color()
+    curses.use_default_colors()
+
+    curses.init_pair(1, - 1, -1)
+    curses.init_pair(2, curses.COLOR_GREEN, -1)
 
     while True:
         stdscr.clear()
 
         stdscr.addstr(1, 2, "Press 'q' to exit")
 
-        add_line_of_text(stdscr, "hi my name is eesa", 10)
+        add_line_of_text("hi my name is eesa", 10, 2)
+        add_line_of_text
 
         key = stdscr.getch()
 
