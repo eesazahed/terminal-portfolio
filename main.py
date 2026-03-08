9cece1d4b442f475da3483ee1e34e13019628c2a
Eesa Zahed <eesa.zahed@gmail.com>
2026-03-07 22:02:55 -0600

    centers text properly

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 from curses import wrapper
 
 
+def add_line_of_text(stdscr, text, y):
+    height, width = stdscr.getmaxyx()
+    center_x = width // 2 - len(text) // 2
+    stdscr.addstr(y, center_x, text)
+
+
 def main(stdscr):
     stdscr.clear()
-    stdscr.addstr(3, 0, "hi")
-    stdscr.addstr(5, 0, "eesa")
-    stdscr.refresh()
-    stdscr.getch()
+    stdscr.nodelay(False)
+    stdscr.keypad(True)
+
+    while True:
+        stdscr.clear()
+
+        stdscr.addstr(1, 2, "Press 'q' to exit")
+
+        add_line_of_text(stdscr, "hi my name is eesa", 10)
+
+        key = stdscr.getch()
+
+        if key == ord('q'):
+            break
+
+        stdscr.refresh()
 
 
 wrapper(main)
